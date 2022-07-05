fe3de241d0a6168529768f519008ec731b35a9a6
Ramzan Shahid <mramzan4422@gmail.com>
2022-07-06 01:27:45 +0500

    fibonacci number again

---
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_number_again.py
@@ -0,0 +1,43 @@
+# python3
+
+
+def fibonacci_number_again_naive(n, m):
+    assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
+
+    if n <= 1:
+        return n
+
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % m
+    return current
+
+
+def pisano_period(m):
+    current, next = 0, 1
+    remainder = 0
+    for i in range( (m*m)+1):
+        remainder = (current + next) % m
+        current = next
+        next = remainder
+        if current == 0 and next == 1:
+            return i+1
+
+
+def fibonacci_number_again(n, m):
+    assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
+
+    remainder = n % pisano_period(m)
+    previous = 0
+    current = 1
+    res = remainder
+    for _ in range(1, remainder):
+        res = (previous + current) % m
+        previous = current
+        current = res
+    return res
+
+
+if __name__ == '__main__':
+    input_n, input_m = map(int, input().split())
+    print(fibonacci_number_again(input_n, input_m))
